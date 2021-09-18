c8739c795d5283b21a15c4a791c8705386304dce
iralitvinchuk <47001760+iralitv@users.noreply.github.com>
2021-09-18 17:17:26 +0300

    [task 1]: Basics

---
+++ routes.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+
+const requestHandler = (req, res) => {
+  const { url, method } = req;
+  if (url === '/') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Enter message</title></head>');
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+    res.write('</html>');
+    return res.end();
+  }
+  
+  if (url === '/message' && method === 'POST') {
+    const body = [];
+    req.on('data', (chunk) => {
+      body.push(chunk);
+    });
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      fs.writeFile('message.txt', message, (err) => {
+        res.status = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+      });
+    });
+  }
+  
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>Some</title></head>');
+  res.write('<body><h1>First page</h1></body>');
+  res.write('</html>');
+  res.end();
+} 
+
+module.exports = requestHandler;
