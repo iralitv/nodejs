e46eb807b63ad238f4eccb4bdb1145a63697f175
iralitvinchuk <47001760+iralitv@users.noreply.github.com>
2021-09-19 19:41:59 +0300

    [unit 4]: nodemon and debugging

--- routes.js
+++ routes.js
@@ -26,7 +26,7 @@ const requestHandler = (req, res) => {
       });
     });
   }
-  
+
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
   res.write('<head><title>Some</title></head>');
